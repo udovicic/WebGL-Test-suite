5edcbc2282c3e57f8de09ef14bc47cdda5595678
Stjepan Udovicic <udovicic.stjepan@gmail.com>
2015-04-26 21:00:52 +0200

    initial JS code

--- js/functions.js
+++ js/functions.js
@@ -12,6 +12,9 @@ jQuery(function() {
     if (Modernizr.localstorage !== true) {
         displayMessage('Browser not supported!', 'Your broser does not support local storage. Changes will not be saved.');
     }
+
+    // Init preview
+    previewInit();
 });
 
 /**
@@ -21,4 +24,56 @@ function displayMessage(title, msg) {
     jQuery("#msgTitle").html(title);
     jQuery("#msgText").html(msg);
     jQuery('#message').modal();
+}
+
+function previewInit() {
+    // Element dimensions
+    var canvasHolder = jQuery("#preview").first();
+    var height = canvasHolder.height();
+    var width = canvasHolder.width();
+
+    // Create GL Context
+    var renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(width, height);
+    canvasHolder.append(renderer.domElement);
+    var scene = new THREE.Scene;
+
+    // Test objects
+    var cubeGeometry = new THREE.BoxGeometry(100, 100, 100);
+    var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x1ec876 });
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.rotation.y = Math.PI * 45 /  180;
+
+    // Dome
+    var skyboxGeometry = new THREE.BoxGeometry(10000, 10000, 10000);
+    var skyboxMaterial = new THREE.MeshBasicMaterial({color: 0x000000, side: THREE.BackSide });
+    var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
+
+    // Light
+    var pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(0, 300, 200);
+
+    // Camera :: FOV, aspect, near, far
+    var camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 10000);
+    camera.position.y = 160;
+    camera.position.z = 400;
+    camera.lookAt(cube.position);
+
+    // Add all to sceen
+    scene.add(cube);
+    scene.add(skybox);
+    scene.add(camera);
+    scene.add(pointLight);
+
+    // Let the games begin
+    function render() {
+        renderer.render(scene, camera);
+
+        cube.rotation.y -= clock.getDelta();
+        requestAnimationFrame(render);
+    }
+    var clock = new THREE.Clock;
+    render();
+
+
 }
