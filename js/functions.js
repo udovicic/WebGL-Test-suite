cc026adead39bd04a0e3a012d7838882155f3ddd
Stjepan Udovicic <udovicic.stjepan@gmail.com>
2015-06-27 22:40:16 +0200

    Refactor code to use objects

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,9 @@
 /**
  * Initialize editor
  */
+
+var preview;
+
 jQuery(function() {
     // Broser support test
     if (Modernizr.canvas !== true) {
@@ -15,44 +18,33 @@ jQuery(function() {
         displayMessage('Browser not supported!', 'Your broser does not support local storage. Changes will not be saved.');
     }
 
-    // Editor elements
-    var vertex_el = jQuery('#vertex-shader');
-    var fragment_el = jQuery('#fragment-shader');
-    var model_el = jQuery('#json-model');
+    // iinitialize preview object
+    preview = new editor(
+        $('#vertex-shader'),
+        $('#fragment-shader'),
+        $('#json-model'),
+        jQuery("#preview")
+    );
+    preview
+        .load()
+        .initContext()
+        .fakeObject()
+        .initLight()
+        .initCamera()
+        .render();
 
-    // Load everything at boot time
-    load(vertex_el, fragment_el, model_el);
 
-    // Add apply button handler
-    jQuery('#btnApply').on('click', function() {
-        save(vertex_el.val(), fragment_el.val(), model_el.val());
-    });
 
-    // Init preview
-    previewInit();
 });
 
 /**
  * Save editor state
  */
-function save(vertex, fragment, model) {
-    localStorage.setItem("vertex", vertex);
-    localStorage.setItem("fragment", fragment);
-    localStorage.setItem("model", model);
-}
-
-/**
- * Load editor state
- */
-function load(vertex_el, fragment_el, model_el) {
-    vertex = localStorage.vertex;
-    fragment = localStorage.fragment;
-    model = localStorage.model;
-
-    if (vertex) vertex_el.val(vertex);
-    if (fragment) fragment_el.val(fragment);
-    if (model) model_el.val(model);
-}
+jQuery('#btnApply').on('click', function() {
+    localStorage.setItem("vertex", vertex_el.val());
+    localStorage.setItem("fragment", ragment_el.val());
+    localStorage.setItem("model", modmodel_el.val());
+});
 
 /**
  * Displays message in modal box
@@ -64,54 +56,88 @@ function displayMessage(title, msg) {
 }
 
 /**
- * Initialzie WebGL
+ * Editor class
  */
-function previewInit() {
-    // Element dimensions
-    var canvasHolder = jQuery("#preview").first();
-    var height = canvasHolder.height();
-    var width = canvasHolder.width();
+function editor(vertex, fragment, model, preview) {
+    this.vertex_el      = vertex;
+    this.fragment_el    = fragment;
+    this.model_el       = model;
+    this.preview_el     = preview.first();
 
-    // Create GL Context
-    var renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(width, height);
-    canvasHolder.append(renderer.domElement);
-    var scene = new THREE.Scene;
+    // Load source code from localstorage
+    this.load = function () {
+        vertex = localStorage.vertex;
+        fragment = localStorage.fragment;
+        model = localStorage.model;
 
-    // Test objects
-    var cubeGeometry = new THREE.BoxGeometry(100, 100, 100);
-    var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x1ec876 });
-    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.rotation.y = Math.PI * 45 /  180;
+        if (vertex)     this.vertex_el.val(vertex);
+        if (fragment)   this.fragment_el.val(fragment);
+        if (model)      this.model_el.val(model);
 
-    // Dome
-    var skyboxGeometry = new THREE.BoxGeometry(10000, 10000, 10000);
-    var skyboxMaterial = new THREE.MeshBasicMaterial({color: 0x000000, side: THREE.BackSide });
-    var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
-
-    // Light
-    var pointLight = new THREE.PointLight(0xffffff);
-    pointLight.position.set(0, 300, 200);
-
-    // Camera :: FOV, aspect, near, far
-    var camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 10000);
-    camera.position.y = 160;
-    camera.position.z = 400;
-    camera.lookAt(cube.position);
-
-    // Add all to sceen
-    scene.add(cube);
-    scene.add(skybox);
-    scene.add(camera);
-    scene.add(pointLight);
-
-    // Let the games begin
-    function render() {
-        renderer.render(scene, camera);
-
-        cube.rotation.y -= clock.getDelta();
-        requestAnimationFrame(render);
+        return this;
     }
-    var clock = new THREE.Clock;
-    render();
+
+    // Initialize WebGL context
+    this.initContext = function() {
+        // Element dimensions
+        var height = this.preview_el.height();
+        var width = this.preview_el.width();
+
+        // Create GL Context
+        this.gl_renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.gl_renderer.setSize(width, height);
+        this.preview_el.append(this.gl_renderer.domElement);
+
+        // Create GL scene
+        this.gl_scene = new THREE.Scene;
+
+        // Cretae GL Clock for controling fps
+        this.gl_clock = new THREE.Clock;
+
+        return this;
+    }
+
+    this.fakeObject = function() {
+        var cubeGeometry = new THREE.BoxGeometry(100, 100, 100);
+        var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x1ec876 });
+        this.gl_object = new THREE.Mesh(cubeGeometry, cubeMaterial);
+        this.gl_object.rotation.y = Math.PI * 45 /  180;
+
+        this.gl_scene.add(this.gl_object);
+
+        return this;
+    }
+
+    this.initLight = function() {
+        this.gl_point_light = new THREE.PointLight(0xffffff);
+        this.gl_point_light.position.set(0, 300, 200);
+
+        this.gl_scene.add(this.gl_point_light);
+
+        return this;
+    }
+
+    this.initCamera = function() {
+        // Element dimensions
+        var height = this.preview_el.height();
+        var width = this.preview_el.width();
+
+        this.gl_camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 10000);
+        this.gl_camera.position.y = 160;
+        this.gl_camera.position.z = 400;
+        this.gl_camera.lookAt(this.gl_object.position);
+
+        this.gl_scene.add(this.gl_camera);
+
+        return this;
+    }
+
+    this.render = function() {
+        this.gl_renderer.render(this.gl_scene, this.gl_camera);
+
+        this.gl_object.rotation.y -= this.gl_clock.getDelta();
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    return this;
 }
